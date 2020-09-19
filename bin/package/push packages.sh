ff1cc458f7208189ea0939762e46c5a38010b797
Vitaliy <theyamshikov@yandex.ru>
2020-09-19 23:29:41 +0600

    upd

--- bin/package/push packages.sh
+++ bin/package/push packages.sh
@@ -1,60 +1,61 @@
 #!/bin/sh
 
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/article && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/messenger && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/notify && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/queue && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/reference && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/storage && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/user && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/dashboard && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/language && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/rbac && git add . && git pull && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/article && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/messenger && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/notify && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/queue && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/reference && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/storage && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/user && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/dashboard && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/language && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znbundle/rbac && git add . && git commit -m upd && git push
 
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zncore/base && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zncore/db && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zncore/domain && git add . && git pull && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zncore/base && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zncore/db && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zncore/domain && git add . && git commit -m upd && git push
 
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zncrypt/base && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zncrypt/jwt && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zncrypt/pki && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zncrypt/tunnel && git add . && git pull && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zncrypt/base && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zncrypt/jwt && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zncrypt/pki && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zncrypt/tunnel && git add . && git commit -m upd && git push
 
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zndoc/linux && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zndoc/rest-api && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zndoc/team && git add . && git pull && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zndoc/linux && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zndoc/rest-api && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zndoc/team && git add . && git commit -m upd && git push
 
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znexample/composer-package && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znexample/database && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znexample/phpunit && git add . && git pull && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znexample/composer-package && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znexample/database && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znexample/phpunit && git add . && git commit -m upd && git push
 
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znfixture/geo && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znfixture/info-bot && git add . && git pull && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znfixture/geo && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znfixture/info-bot && git add . && git commit -m upd && git push
 
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znfork/danog_madelineproto && git add . && git pull && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znfork/danog_madelineproto && git add . && git commit -m upd && git push
 
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znlib/rest && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znlib/socket && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znlib/web && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znlib/console && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znlib/init && git add . && git pull && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znlib/rest && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znlib/socket && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znlib/web && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znlib/console && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znlib/init && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znlib/telegram && git add . && git commit -m upd && git push
 
-#cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znsandbox/bundle && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znsandbox/sandbox && git add . && git pull && git commit -m upd && git push
-#cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znsandbox/telegram && git add . && git pull && git commit -m upd && git push
-#cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znsandbox/web && git add . && git pull && git commit -m upd && git push
-#cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znsandbox/yii2-legacy && git add . && git pull && git commit -m upd && git push
+#cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znsandbox/bundle && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znsandbox/sandbox && git add . && git commit -m upd && git push
+#cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znsandbox/telegram && git add . && git commit -m upd && git push
+#cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znsandbox/web && git add . && git commit -m upd && git push
+#cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znsandbox/yii2-legacy && git add . && git commit -m upd && git push
 
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntool/dev && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntool/test && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntool/rest-client && git add . && git pull && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntool/dev && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntool/test && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntool/rest-client && git add . && git commit -m upd && git push
 
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntpl/backend.vkrg && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntpl/symfony4 && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntpl/telegram-bot && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntpl/telegram-client && git add . && git pull && git commit -m upd && git push
-#cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntpl/yii2 && git add . && git pull && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntpl/backend.vkrg && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntpl/symfony4 && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntpl/telegram-bot && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntpl/telegram-client && git add . && git commit -m upd && git push
+#cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/zntpl/yii2 && git add . && git commit -m upd && git push
 
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znyii/base && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znyii/app && git add . && git pull && git commit -m upd && git push
-cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znyii/error && git add . && git pull && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znyii/base && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znyii/app && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/telegram-bot/vendor/znyii/error && git add . && git commit -m upd && git push
