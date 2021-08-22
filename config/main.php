a183178091c8985e5bf56679668f640295c6b4a2
Vitaliy <theyamshikov@yandex.ru>
2021-08-22 17:55:11 +0600

    upd

--- config/main.php
+++ config/main.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-   /* 'telegram' => [
+    'telegram' => [
         'bot' => [
             'hookUrl' => 'https://tlgbot12.000webhostapp.com',
             'token' => $_ENV['TELEGRAM_BOT_TOKEN'],
             'timeout' => 30,
         ],
-    ],*/
+    ],
 ];
